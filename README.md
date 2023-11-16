19763719a94a52878fb4797fddd592745d7200db
jawad shaikh <shaikhjawad314@gmail.com>
2023-11-16 12:02:37 +0500

    upudated UIU

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Tab-Importer-Exporter
+# Tab Importer n Exporter
+
+## Description
+
+"Tab Importer n Exporter" is your go-to solution for efficient tab management in Chrome. Inspired by the need to seamlessly transition between work and home environments, this extension empowers you to effortlessly export and import your tab configurations, ensuring a smooth continuation of your browsing experience across devices.
+
+### Key Features
+
+- **Export/Import Tabs:** Save your active tab sessions as easily shareable files. Whether it's transferring work-related tabs from the office to home or simply keeping a backup, "Tab Importer n Exporter" export/import functionality has you covered.
+
+- **Memory Optimization:** Tired of excessive RAM usage due to numerous open tabs? TabSync allows you to export groups of tabs, providing a quick way to declutter your browser without losing valuable information.
+
+## How "Tab Importer n Exporter" Can Benefit You
+
+### Work-Life Transition
+
+- **Scenario:** You're working on a project at the office, and you have multiple tabs open with relevant resources and information.
+- **Use Case:** Use "Tab Importer n Exporter" to export your current tab group before leaving the office. When you get home, import the tabs, and seamlessly continue your work with all the resources intact.
+
+### Memory Management
+
+- **Scenario:** You often find your Chrome browser slowing down due to the multitude of open tabs, consuming significant RAM.
+- **Use Case:** Export groups of tabs that you don't need at the moment to optimize memory usage. Import them back when necessary, allowing you to maintain a lean browsing experience.
+
+### Backup Important Sessions
+
+- **Scenario:** You have a set of tabs containing valuable information or ongoing projects, and you want to ensure you don't lose them.
+- **Use Case:** Before closing a set of critical tabs, export them using "Tab Importer n Exporter". Create a backup folder on your computer or cloud storage, providing a reliable archive of essential browsing sessions.
+
+### Sharing Research or Resources
+
+- **Scenario:** You come across a collection of useful websites, articles, or research materials that you want to share with a colleague or friend.
+- **Use Case:** Use "Tab Importer n Exporter" to export the relevant tab group and share the exported file. The recipient can then import the file into their browser, instantly accessing the same set of resources.
+
+### Streamlining Task-Oriented Browsing
+
+- **Scenario:** You have a habit of opening specific sets of tabs for different tasks or projects.
+- **Use Case:** Create and export tab groups tailored to different tasks (e.g., "Work Projects," "Research," "Personal Tasks"). Import the relevant group based on your current focus, streamlining your workflow.
+
+## Get Started
+
+Say goodbye to the hassle of manually copying tab links or worrying about excessive RAM usage. With "Tab Importer n Exporter", your tabs are securely stored, ready to be exported or imported at your convenience. Download the extension now and unlock a new level of productivity in your browsing journey.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE.md).
--- popup.js
+++ popup.js
@@ -1,7 +1,91 @@
-const tabGroups = {};
+populateTabs();
 
-const download = (dataToDownload) => {
-  const data = JSON.stringify(dataToDownload, null, 2);
+function getTabGroups() {
+  const tabGroups = {};
+
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({}, function (tabs) {
+      tabs.forEach((tab) => {
+        if (!tabGroups.hasOwnProperty(tab.groupId)) {
+          tabGroups[tab.groupId] = [];
+        }
+  
+        tabGroups[tab.groupId].push(tab);
+      });
+
+      resolve(tabGroups);
+    });
+  });
+};
+
+async function populateTabs() {
+  const tabGroups = await getTabGroups();
+
+  for (const groupId in tabGroups) {
+    renderData(groupId, tabGroups[groupId]);
+  }
+};
+
+function renderData(groupId, tabGroup) {
+  const container = document.createElement('div');
+  container.style.marginBottom = '20px';
+  container.style.border = '1px solid #ccc';
+  container.style.padding = '10px';
+
+  const table = document.createElement('table');
+  table.style.width = '100%';
+  table.style.borderCollapse = 'collapse';
+  table.style.marginTop = '10px';
+  table.innerHTML = `
+    <thead>
+      <tr>
+        <th style="border: 1px solid #ddd; padding: 8px;">
+          Tab Group
+          <img
+            src="images/download-icon.svg"
+            width="15px"
+          >
+        </th>
+      </tr>
+    </thead>
+    <tbody></tbody>
+  `;
+
+  const tbody = table.querySelector('tbody');
+  const downloadIcon = table.querySelector('thead tr th img');
+
+  downloadIcon.addEventListener("click", () => download(groupId));
+
+  tabGroup.forEach((tab) => {
+    const row = document.createElement('tr');
+    row.style.border = '1px solid #ddd';
+    row.style.padding = '8px';
+
+    row.innerHTML = `
+      <td style="border: 1px solid #ddd; padding: 8px;">
+        <abbr title="${tab.url}">
+          <img src="${tab.favIconUrl}" width="15px">
+          ${tab.title}
+        </abbr>
+      </td>
+    `;
+
+    tbody.appendChild(row);
+  });
+
+  container.appendChild(table);
+  document.body.appendChild(container);
+};
+
+async function download(groupId) {
+  const { [groupId]: group } = await getTabGroups();
+
+  const transformedGroup = group.map((i) => {
+    return { favIconUrl: i.favIconUrl, title: i.title, url: i.url };
+  })
+
+  const data = JSON.stringify(transformedGroup, null, 2);
+
   const blob = new Blob([data], { type: "application/json" });
   const url = URL.createObjectURL(blob);
 
@@ -14,60 +98,41 @@ const download = (dataToDownload) => {
 };
 
 const createTabs = async (tabList) => {
-  const tabsIds = [];
+  const tabIds = [];
 
-  for (const _tab of tabList) {
-    const tab = await chrome.tabs.create({ url: _tab.url });
-    tabsIds.push(tab.id);
+  for (const tabInfo of tabList) {
+    const createdTab = await chrome.tabs.create({ url: tabInfo.url });
+    tabIds.push(createdTab.id);
   }
 
-  const groupId = await chrome.tabs.group({ tabIds: tabsIds });
+  const groupId = await chrome.tabs.group({ tabIds });
 
   chrome.tabGroups.update(groupId, {
     collapsed: true,
-    title: "yoyo",
+    title: "importer",
     color: "grey",
   });
 };
 
-document.getElementById("getTabs").addEventListener("click", function () {
-  chrome.tabs.query({}, function (tabs) {
-    tabs.forEach((tab) => {
-      if (!tabGroups.hasOwnProperty(tab.groupId)) {
-        tabGroups[tab.groupId] = [];
-      }
 
-      tabGroups[tab.groupId].push(tab);
-    });
-
-    console.log("tabGroups", tabGroups);
-  });
-});
-
-document.getElementById("getTab").addEventListener("click", function () {
-  const groupId = document.getElementById("tabId").value;
-
-  const group = tabGroups[groupId];
-  download(
-    group.map((i) => {
-      return { favIconUrl: i.favIconUrl, title: i.title, url: i.url };
-    })
-  );
-});
-
-document
-  .getElementById("openTabsButton")
-  .addEventListener("click", function () {
-    const fileInput = document.getElementById("fileInput");
-    const file = fileInput.files[0];
-
+function readFile(file) {
+  return new Promise((resolve, reject) => {
     const reader = new FileReader();
 
+    reader.readAsText(file);
     reader.onload = function (e) {
       const tabList = JSON.parse(e.target.result);
-
-      createTabs(tabList);
+  
+      resolve(tabList);
     };
+  })
+}
 
-    reader.readAsText(file);
-  });
+document.getElementById("openTabsButton").addEventListener("click", async function () {
+  const fileInput = document.getElementById("fileInput");
+  const file = fileInput.files[0];
+
+  const tabList = await readFile(file);
+
+  createTabs(tabList);
+});
